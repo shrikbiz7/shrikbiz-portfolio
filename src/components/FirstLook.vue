e40a83f160ab50f3f83e0098ef20765ea63621fb
shrikbiz7 <shrikant.patel@centurylink.com>
2021-03-19 13:11:16 -0600

    seperate component

---
+++ src/components/FirstLook.vue
@@ -0,0 +1,98 @@
+<template>
+    <v-row v-scroll:#scroll-target="onScroll" justify="center" class="masterhead">
+        <v-col cols="auto">
+            <div style="width: 100%; height: 500px; overflow-y: hidden; overflow-x: hidden">
+                <v-img :height="imgHeight" style="margin: 10rem 0; overflow-x: hidden" src="@/assets/darkthemeWP.jpeg">
+                    <div width="100%" style="color:white" align="center">
+                        <h1
+                            :style="{
+                                marginTop: '50px',
+                                fontFamily: 'Helvetica',
+                                width: '100%',
+                                overflowX: 'hidden',
+                            }"
+                        >
+                            <span
+                                v-for="(letter, index) in 'ShrikantPatel'"
+                                :key="index"
+                                :style="{
+                                    fontSize: titleSize + 'px',
+                                    marginLeft: titleSpacing + 'px',
+                                }"
+                            >
+                                {{ letter }}
+                            </span>
+                        </h1>
+                        <h4
+                            align="center"
+                            :style="{
+                                width: '100%',
+                                overflowX: 'hidden',
+                                fontSize: subTitleSize + 'px',
+                            }"
+                        >
+                            FrontEnd Software Engineer
+                        </h4>
+                    </div>
+                </v-img>
+            </div>
+            <v-container>
+                <p style="font-size: 2rem; text-align: center; margin-bottom: 2rem">
+                    Software Developer | Web Designer | Algorithm Enthusiastic | rookie Content Creator | Former Soccer
+                    Player | Painter | Cook | Star Wars | Marvels | Game of Thrones | Gym Enthusiastic
+                </p>
+                <p style="text-align: center">
+                    Hi, I am Shrikant Patel from Denver, Colorado with ~3 years of experience as FrontEnd Software
+                    Engineer. Event though first framework that I learned was Angular, I generally work on Vue and
+                    React. When I am not programming, I like to go for hiking, cooking, or watch few Real Madrid or
+                    Juventus games. I have currently started to create content on FrontEnd Development related topics
+                    for Instagram, and soon for YouTube.
+                </p>
+            </v-container>
+        </v-col>
+    </v-row>
+</template>
+
+<script lang="ts">
+import vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class FirstLook extends vue {
+    scrollVariable: number = 0;
+
+    onScroll(event: any) {
+        this.scrollVariable = event.target.scrollTop;
+    }
+    get imgHeight() {
+        let number: number = this.scrollVariable;
+        let constant = 400;
+        constant += number;
+        return constant ? constant : 0;
+    }
+    get titleSize(): number {
+        let number: number = this.scrollVariable / 45;
+        number += 100;
+        return number;
+    }
+    get titleSpacing(): number {
+        let number: number = this.scrollVariable / 10;
+        return number;
+    }
+    get titleSpeed(): number {
+        let constant: number = 860;
+        return this.scrollVariable > constant ? Math.pow(this.scrollVariable - constant, 1.35) : 0;
+    }
+    get subTitleSpeed(): number {
+        let constant: number = 860;
+        return this.scrollVariable > constant ? Math.pow(this.scrollVariable - constant, 1.2) : 0;
+    }
+    get subTitleSize(): number {
+        let number: number = this.scrollVariable / 50;
+        let constant = 50;
+        constant -= number;
+        return constant;
+    }
+}
+</script>
+<style></style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,65 +4,7 @@
         style="max-height: calc(100vh - 56.8px); width: 100%; height: auto; overflow-x: hidden"
         class="overflow-y-auto"
     >
-        <div>
-            <v-row v-scroll:#scroll-target="onScroll" justify="center" class="masterhead">
-                <v-col cols="auto">
-                    <div style="width: 100%; height: 500px; overflow-y: hidden; overflow-x: hidden">
-                        <v-img
-                            :height="imgHeight"
-                            style="margin: 10rem 0; overflow-x: hidden"
-                            src="@/assets/darkthemeWP.jpeg"
-                        >
-                            <div width="100%" style="color:white" align="center">
-                                <h1
-                                    :style="{
-                                        marginTop: '50px',
-                                        fontFamily: 'Helvetica',
-                                        width: '100%',
-                                        overflowX: 'hidden',
-                                    }"
-                                >
-                                    <span
-                                        v-for="(letter, index) in 'ShrikantPatel'"
-                                        :key="index"
-                                        :style="{
-                                            fontSize: titleSize + 'px',
-                                            marginLeft: titleSpacing + 'px',
-                                        }"
-                                    >
-                                        {{ letter }}
-                                    </span>
-                                </h1>
-                                <h4
-                                    align="center"
-                                    :style="{
-                                        width: '100%',
-                                        overflowX: 'hidden',
-                                        fontSize: subTitleSize + 'px',
-                                    }"
-                                >
-                                    FrontEnd Software Engineer
-                                </h4>
-                            </div>
-                        </v-img>
-                    </div>
-                    <v-container>
-                        <p style="font-size: 2rem; text-align: center; margin-bottom: 2rem">
-                            Software Developer | Web Designer | Algorithm Enthusiastic | rookie Content Creator |
-                            ex-Footballer (Soccer) | Painter | Cook | Star Wars | Marvels | Game of Thrones | Gym
-                            Enthusiastic
-                        </p>
-                        <p style="text-align: center">
-                            Hi, I am Shrikant Patel from Denver, Colorado with ~3 years of experience as FrontEnd
-                            Software Engineer. Event though first framework that I learned was Angular, I generally work
-                            on Vue and React. When I am not programming, I like to go for hiking, cooking, or watch few
-                            Real Madrid or Juventus games. I have currently started to create content on FrontEnd
-                            Development related topics for Instagram, and soon for YouTube.
-                        </p>
-                    </v-container>
-                </v-col>
-            </v-row>
-        </div>
+        <FirstLook />
         <v-row v-scroll:#scroll-target="onScroll" justify="center" class="secondHead primary">
             <v-col cols="auto">
                 <div align="center" style="margin-top: 10rem">
@@ -95,7 +37,7 @@
                         align="center"
                         :style="{
                             width: '100%',
-                            marginLeft: subTitleSpeed + 'px',
+                            marginLeft: titleSpeed + 'px',
                             overflowX: 'none',
                         }"
                     >
@@ -117,6 +59,7 @@ import { Characters } from '@/constants/char';
 @Component({
     components: {
         Roller: () => import('vue-roller'),
+        FirstLook: () => import('@/components/FirstLook.vue'),
     },
 })
 export default class HomePage extends Vue {
@@ -133,11 +76,12 @@ export default class HomePage extends Vue {
     }
 
     get titleSpeed(): number {
-        let constant: number = 860;
-        return this.scrollVariable > constant ? Math.pow(this.scrollVariable - constant, 1.35) : 0;
+        let constant: number = 862;
+        let number = constant - this.scrollVariable;
+        return number > 0 ? number : 0;
     }
     get subTitleSpeed(): number {
-        let constant: number = 860;
+        let constant: number = 862;
         return this.scrollVariable > constant ? Math.pow(this.scrollVariable - constant, 1.2) : 0;
     }
 
@@ -147,30 +91,6 @@ export default class HomePage extends Vue {
         let charList: string[] = [];
         charList.push(...Characters, ...capsCharList, ...specialChar, ' ');
         return charList;
-    }
-
-    get imgHeight() {
-        let number: number = this.scrollVariable;
-        let constant = 400;
-        constant += number;
-        return constant ? constant : 0;
-    }
-
-    get titleSize(): number {
-        let number: number = this.scrollVariable / 45;
-        number += 100;
-        return number;
-    }
-    get subTitleSize(): number {
-        let number: number = this.scrollVariable / 50;
-        let constant = 50;
-        constant -= number;
-        return constant;
-    }
-
-    get titleSpacing(): number {
-        let number: number = this.scrollVariable / 10;
-        return number;
     }
 
     onScroll(event: any) {
